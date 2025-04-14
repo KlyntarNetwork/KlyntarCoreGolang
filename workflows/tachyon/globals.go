398556e6edcd6a5a3a38d658b1258d313f7eab35
VladChernenko <vladchernenko@protonmail.com>
2025-04-14 03:03:56 +0300

    Choose the best way of race condition prevention (via pointers), added test + some refactoring to globals

--- workflows/tachyon/globals.go
+++ workflows/tachyon/globals.go
@@ -40,33 +40,19 @@ var CORE_MAJOR_VERSION uint = func() uint {
 
 var CHAINDATA_PATH, GENESIS_PATH, CONFIGS_PATH string
 
-// Global configs (resolved by <CONFIGS_PATH>, example available in workflows/tachyon/templates/configs.json)
+var CONFIGS, GENESIS map[string]interface{}
 
-var CONFIGS map[string]interface{}
-
-// Load genesis from JSON file to pre-set the state
-
-var GENESIS map[string]interface{}
-
-var GLOBAL_CACHES = struct {
-	MEMPOOL struct {
-		slice []Transaction
-		mutex sync.RWMutex
-	}
-
-	APPROVEMENT_THREAD_CACHE map[string]interface{}
-}{
-	APPROVEMENT_THREAD_CACHE: make(map[string]interface{}),
+var MEMPOOL struct {
+	slice []Transaction
+	mutex sync.RWMutex
 }
+
+var APPROVEMENT_THREAD_CACHE = make(map[string]interface{})
 
 var GENERATION_THREAD threads.GenerationThread
 
 var APPROVEMENT_THREAD threads.ApprovementThread
 
-var BLOCKCHAIN_DATABASES = struct {
-	BLOCKS, EPOCH_DATA, APPROVEMENT_THREAD_METADATA, FINALIZATION_VOTING_STATS *leveldb.DB
-}{
-	nil, nil, nil, nil,
-}
+var BLOCKS, EPOCH_DATA, APPROVEMENT_THREAD_METADATA, FINALIZATION_VOTING_STATS *leveldb.DB = nil, nil, nil, nil
 
 var VOTING_REQUESTS chan struct{}
--- workflows/tachyon/threads/generation_thread.go
+++ workflows/tachyon/threads/generation_thread.go
@@ -1,8 +1,10 @@
 package threads
 
 type GenerationThread struct {
-	EpochFullId string `json:"epochFullId"`
-	EpochIndex  int    `json:"epochIndex"`
-	PrevHash    string `json:"prevHash"`
-	NextIndex   int    `json:"nextIndex"`
+	EpochFullId string   `json:"epochFullId"`
+	EpochIndex  int      `json:"epochIndex"`
+	PrevHash    string   `json:"prevHash"`
+	NextIndex   int      `json:"nextIndex"`
+	Quorum      []string `json:"quorum"`
+	Majority    uint     `json:"majority"`
 }
