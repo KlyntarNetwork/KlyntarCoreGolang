d5160ef7ceddd8fbc5cbf94a8c5506e81f2e9213
VladChernenko <vladchernenko@protonmail.com>
2025-05-08 02:03:40 +0300

    Scaffold for last 2 threads. Mostly finished with share_block_and_grab_proofs (4/5 function)

--- life/share_block_and_grab_proofs.go
+++ life/share_block_and_grab_proofs.go
@@ -1,9 +1,21 @@
 package life
 
 import (
+	"encoding/json"
+	"strconv"
+	"time"
+
+	"github.com/KlyntarNetwork/KlyntarCoreGolang/globals"
 	"github.com/KlyntarNetwork/KlyntarCoreGolang/structures"
 	"github.com/gorilla/websocket"
 )
+
+type ProofsGrabber struct {
+	EpochId        int
+	AcceptedIndex  int
+	AcceptedHash   string
+	AfpForPrevious structures.AggregatedFinalizationProof
+}
 
 var WEBSOCKET_CONNECTIONS map[string]*websocket.Conn
 
@@ -11,13 +23,65 @@ var FINALIZATION_PROOFS_CACHE map[string]string
 
 var RESPONSES chan Agreement
 
-var PROOFS_GRABBER struct {
-	EpochId        string
-	AcceptedIndex  int
-	AcceptedHash   string
-	AfpForPrevious structures.AggregatedFinalizationProof
+var PROOFS_GRABBER = ProofsGrabber{
+	EpochId: -1,
 }
 
 func processIncomingFinalizationProof(msg []byte) {}
 
-func BlocksSharingAndProofsGrabingThread() {}
+func BlocksSharingAndProofsGrabingThread() {
+
+	globals.APPROVEMENT_THREAD.RWMutex.RLock()
+
+	defer globals.APPROVEMENT_THREAD.RWMutex.RUnlock()
+
+	epochHandler := globals.APPROVEMENT_THREAD.Thread.EpochHandler
+
+	currentLeaderPubKey := epochHandler.LeadersSequence[epochHandler.CurrentLeaderIndex]
+
+	if currentLeaderPubKey != globals.CONFIGURATION.PublicKey {
+
+		time.AfterFunc(2*time.Second, func() {
+			BlocksSharingAndProofsGrabingThread()
+		})
+
+		return
+
+	}
+
+	if PROOFS_GRABBER.EpochId != epochHandler.Id {
+
+		// Try to get stored proofs grabber from db
+
+		dbKey := []byte(strconv.Itoa(epochHandler.Id) + ":PROOFS_GRABBER")
+
+		if rawGrabber, err := globals.FINALIZATION_VOTING_STATS.Get(dbKey, nil); err != nil {
+
+			json.Unmarshal(rawGrabber, &PROOFS_GRABBER)
+
+		} else {
+
+			// Assign initial value of proofs grabber for each new epoch
+
+			PROOFS_GRABBER = ProofsGrabber{
+
+				EpochId: epochHandler.Id,
+
+				AcceptedIndex: -1,
+
+				AcceptedHash: "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+			}
+
+		}
+
+		// And store new descriptor
+
+		if serialized, err := json.Marshal(PROOFS_GRABBER); err == nil {
+
+			globals.FINALIZATION_VOTING_STATS.Put(dbKey, serialized, nil)
+
+		}
+
+	}
+
+}
